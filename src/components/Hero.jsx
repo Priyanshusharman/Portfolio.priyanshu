b9b540b9fb9c16f5a81847eb2ca2f454c1c058c3
priyanshusharman <priyanshuk1900@gmaul.com>
2023-10-29 15:07:13 +0530

    add a car and moniter animation withresponsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
-
+import { motion } from "framer-motion"
+import { styles } from "../style"
+import { ComputersCanvas } from "./canvas/index"
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='relative w-full h-screen ax-auto'>
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7x1 mx-auto flex flex-row items-start gap-5`}>
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 square-full bg-[#4cbd49]' />
+          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white block`}>HI ,I'am <span className='text-[#45a14b]'>Priyanshu</span></h1>
+          <p className={`${styles.herosubText} mt-2 text-white-100`}> I am currently study in <br className='sm:block hidden' /> JAYPEE UNIVERSITY</p>
+        </div>
+      </div>
+      <ComputersCanvas />
+      <div className='absolute xs:bootom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href="#about"><div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+          <motion.div
+            animate={{ y: [0, 24, 0] }}
+            transition={{
+              duration: 1.5,
+              repeat: Infinity,
+              repeatType: 'loop'
+            }}
+            className='w-3 h-3 rounded-full bg-secondary mb-1' />
+        </div></a>
+
+      </div>
+    </section>
   )
 }
 
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
-
+import { Suspense, useEffect, useState } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, meshBounds, useGLTF } from '@react-three/drei'
+import Loader from "../Loader"
 const Computers = () => {
+  const computer = useGLTF('./desktop_pc/scene.gltf')
+  const car = useGLTF('./desktop_pc/old_rusty_car/scene.gltf')
+  const [ismobile,setmobile]=useState(false);
+  useEffect(()=>{
+    const mediaquary =window.matchMedia('(max-width:600px)');
+    setmobile(mediaquary.matches);
+    
+    const handlemediaquerychange=(event)=>{
+      setmobile(event.matches);
+    }
+    mediaquary.addEventListener('change',handlemediaquerychange);
+    return()=>{
+      mediaquary.removeEventListener('change',handlemediaquerychange);
+    }
+  },[])
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={2.5}
+        groundColor="black" />
+      <pointLight intensity={1} />
+      <spotLight intensity={1.8}/>
+      <primitive
+        object={car.scene}
+        // object={computer.scene}
+        scale={ismobile?0.009:0.015}
+        position={[0, -2.25, 0]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
+  )
+}
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<Loader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI /2 }
+          minPolarAngle={Math.PI / 3}
+        />
+        <Computers />
+      </Suspense>
+      <Preload all />
+    </Canvas>
   )
 }
 
-export default Computers
+export default ComputersCanvas
